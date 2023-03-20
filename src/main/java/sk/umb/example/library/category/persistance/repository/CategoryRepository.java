94e8e9e1e6888999676832515990af36ec1ef9f8
LucasJF14 <flasik.lukas@gmail.com>
2023-03-20 16:18:47 +0100

    FunkcneCategory

--- src/main/java/sk/umb/example/library/category/persistance/repository/CategoryRepository.java
+++ src/main/java/sk/umb/example/library/category/persistance/repository/CategoryRepository.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends CrudRepository<CategoryEntity, Long> {
-    Optional<CategoryEntity> findAll(String category);
 
     Iterable<CategoryEntity> findByCategory(String category);
 }
